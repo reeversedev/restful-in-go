cd8acd4d7cd47d07e634fe5b1e5f8cce2312a900
Prateek Gogia <gogia.prateek@hotmail.com>
2020-05-31 21:46:13 +0530

    JSON response

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+// Article type
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+// Articles type
+type Articles []Article
+
+// AllArticles Handler
+func AllArticles(w http.ResponseWriter, r *http.Request) {
+
+	articles := Articles{
+		Article{Title: "Hello World", Desc: "Hello to the world", Content: "I said, Hello!"},
+	}
+
+	fmt.Println("Endpoint Hit : All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 // HomePage handler
 func HomePage(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +35,7 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/articles", AllArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
